32271bfa4493372de2cb6b20cac64b22c59a5948
lmndye <dustyboy170@gmail.com>
2025-07-18 09:46:32 +0300

    new

--- fvoa.cpp
+++ fvoa.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 int x, y, m;
 float r, r2, r3, y2;
+char again;
 int main(){
+	do {
 	cout << "enter the number of years: ";
 	cin >> y;
 	cout << "enter the amount of money: ";
@@ -17,6 +19,7 @@ int main(){
 	cin >> r;
 	r2 = r / 12, r3 = pow((r2 + 1),y2), r3 = (r3 - 1)/r2;
 	x = r3 * m;
-	cout << x;
+	cout << x << endl, cout << "do you want to calculate again? (y/n)\n", cin >> again;
+	} while (again == 'y' || again == 'y');
 	return 0;
 }
